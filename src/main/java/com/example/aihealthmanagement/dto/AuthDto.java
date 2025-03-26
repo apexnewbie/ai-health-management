a582890c56a343f56f1a395494dcf3f81cc733bb
apexnewbie <2787995041zxj@gmail.com>
2025-03-26 22:29:16 +0800

    Initial commit

---
+++ src/main/java/com/example/aihealthmanagement/dto/AuthDto.java
@@ -0,0 +1,25 @@
+package com.example.aihealthmanagement.dto;
+
+import lombok.Data;
+
+public class AuthDto {
+
+    @Data
+    public static class RegisterRequest {
+        private String username;
+        private String password;
+        private String email;
+    }
+
+    @Data
+    public static class LoginRequest {
+        private String username;
+        private String password;
+    }
+
+    @Data
+    public static class LoginResponse {
+        private String token;
+    }
+}
+
